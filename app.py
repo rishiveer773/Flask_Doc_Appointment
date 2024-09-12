a3d0863fbdda89adc59a1c1292c315ca3fa540ce
JackSparrow9875 <rishiveer07@outlook.com>
2024-09-12 14:58:00 +0530

    Improvements made to the error handling ability of the code

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
-from flask import Flask, render_template, url_for, get_flashed_messages()
+from flask import Flask, render_template, request, url_for, get_flashed_messages, session
 
-#test push
 __name__ = "__main__"
-
 app = Flask(__name__)
+
+app.secret_key = "test_push"
 
 #--------------ADDING ROUTES---------------------------
 @app.route('/')
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/userlogin/<username>')
-def userlogin(username):
-    return render_template('userlogin.html', username=username)
+@app.route('/userlogin/', methods = ['POST', 'GET'])
+def userlogin():
+    error = None
+    if request.method == "POST":
+        if valid_login
+    return render_template('userlogin.html', error = error)
 
-@app.route('/adminlogin')
+@app.route('/adminlogin', methods = ['POST', 'GET'])
 def adminlogin():
     return render_template('adminlogin.html')
 
+
+#---------------ERROR HANDLERS-------------------------
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+#---------------RUN THE CODE---------------------------
 if __name__ == "__main__":
     app.run(debug = True)
